cffbae12364f660c86bf5aef9e108b8eb34499f0
Ozb1coco <ozb1coco2013@mail.ru>
2022-11-07 20:40:40 +0300

    Lesson7.2

--- src/TeachMeSkills/com/Lesson7/Homework/Division.java
+++ src/TeachMeSkills/com/Lesson7/Homework/Division.java
@@ -1,9 +1,9 @@
 package TeachMeSkills.com.Lesson7.Homework;
 
-public class Division implements Calculatble{
+public class Division implements Calculatble {
 
     @Override
     public void action(double number1, double number2) {
-            System.out.println(number1 / number2);
+        System.out.println(number1 / number2);
     }
 }
--- src/TeachMeSkills/com/Lesson7/Homework/Menu.java
+++ src/TeachMeSkills/com/Lesson7/Homework/Menu.java
@@ -1,15 +1,17 @@
 package TeachMeSkills.com.Lesson7.Homework;
 
+import jdk.jfr.StackTrace;
+
 import java.util.Scanner;
 
 public class Menu {
     Scanner scanner = new Scanner(System.in);
-    public void print()
-    {
-        menu();
+
+    public void print() {
+            menu();
     }
-    private void mathOperation(int calculateOperation)
-    {
+
+    private void mathOperation(int calculateOperation) {
         double number1 = 0;
         double number2 = 0;
         if (scanner.hasNextDouble()) {
@@ -33,35 +35,30 @@ public class Menu {
                 break;
             case 4:
                 Calculatble division = new Division();
-                if(number2 == 0)
-                {
-                    System.out.println("U cant dived by zero, try again");
-                }
-                else {
                     division.action(number1, number2);
-                }
-                break;
+                    break;
         }
     }
+
     private void menu() {
         System.out.println("Welcome to calculate program if choose ur action: \n 1) Calculate number \n 2) Exit the program");
         int choose = scanner.nextInt();
         if (choose == 1) {
             for (; ; ) {
                 System.out.println("Enter operation to calculate: \n 1) Summ \n 2) Substraction \n 3) Multiply \n 4) Division  \n 5) Exit");
-                int calculateOperation = scanner.nextInt();
-                if (calculateOperation > 0 && calculateOperation < 6) {
-                    if (calculateOperation == 5) {
-                        System.out.println("U exit from program good bye");
-                        break;
-                    } else {
-                        System.out.println("Enter two numbers: ");
-                        mathOperation(calculateOperation);
+                    int calculateOperation = scanner.nextInt();
+                    if (calculateOperation > 0 && calculateOperation < 6) {
+                        if (calculateOperation == 5) {
+                            System.out.println("U exit from program good bye");
+                            break;
+                        } else {
+                            System.out.println("Enter two numbers: ");
+                            mathOperation(calculateOperation);
+                        }
                     }
-                }
+
             }
-        }
-        else if (choose == 2) {
+        } else if (choose == 2) {
             System.out.println("U exit from calculator good bye");
         } else {
             System.out.println("U entered wrong value");
